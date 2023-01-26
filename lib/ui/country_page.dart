fce1bdda1f86b723c99f4ab6e8f931c34308829c
Samandar <samandarahadjonov@gmail.com>
2023-01-26 15:14:27 +0500

    done

--- lib/ui/country_page.dart
+++ lib/ui/country_page.dart
@@ -1,7 +1,9 @@
+import 'package:flag/flag_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphql_task/data/models/country_model/country_model.dart';
 import 'package:graphql_task/state_managers/cubit/country_cubit.dart';
+import 'package:country_icons/country_icons.dart';
 
 class CountryPage extends StatelessWidget {
   CountryModel countryModel;
@@ -21,7 +23,18 @@ class CountryPage extends StatelessWidget {
                 context.read<CountryCubit>().getCountry(countryModel.code);
               }
               if(state.status==Status.SUCCESS){
-                return Center(child: Text(state.country!.name,));
+                return Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Flag.fromString('${countryModel.code}', height: null, width: null),
+                      Text("Country:  "+countryModel.name,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),),
+                      Text("Langiage:  "+countryModel.native,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500)),
+                      Text("Phone code:  "+countryModel.phone,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500)),
+
+                    ],
+                  ),
+                );
               }
               return const CircularProgressIndicator();
             },
